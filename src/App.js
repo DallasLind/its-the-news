6387bd17d3d9a08b1a15a249b265e3d072444261
DallasLind <mcgraw.dallas@gmail.com>
2020-02-04 09:52:34 -0500

    API is mapped and the articles are listed out and now moving on to styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+const url =
+  "https://newsapi.org/v2/top-headlines?country=us&apiKey=fa57b51843bc4bfb9e79d6723e7b7e6a";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      articles: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(url)
+      .then(data => data.json())
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            articles: result.articles
+          });
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
+
+  render() {
+    const { error, isLoaded, articles } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1>It's the news!</h1>
+          </header>
+          <ul>
+            {articles.map(article => (
+              <li>
+                <a href={article.url} target="_blank" rel="noopener noreferrer">
+                  {article.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
